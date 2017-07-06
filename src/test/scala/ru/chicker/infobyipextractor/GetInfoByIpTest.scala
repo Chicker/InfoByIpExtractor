87082cf29c21f9cd877b2a46c17669c32d6ed365
Chicker <vadim.agishev@gmail.com>
2017-07-06 18:14:15 +0500

    Repair tests

--- src/test/scala/ru/chicker/infobyipextractor/GetInfoByIpTest.scala
+++ src/test/scala/ru/chicker/infobyipextractor/GetInfoByIpTest.scala
@@ -21,7 +21,11 @@ import cats.data.Reader
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FunSpec, Matchers}
 import ru.chicker.infobyipextractor.env.{Env, ProductionEnv}
-import ru.chicker.infobyipextractor.infoprovider.{InfoByIpFreeGeoIpProvider, InfoByIpIp2IpProvider, InfoByIpProvider}
+import ru.chicker.infobyipextractor.infoprovider.{
+  InfoByIpFreeGeoIpProvider,
+  InfoByIpIp2IpProvider,
+  InfoByIpProvider
+}
 import ru.chicker.infobyipextractor.service._
 import ru.chicker.infobyipextractor.util.HttpWeb
 
@@ -29,25 +33,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
-
 class GetInfoByIpTest extends FunSpec with Matchers with MockFactory {
   it("should return country code for known ip-address. Using IpByIp") {
     object testEnv extends ProductionEnv {
       // language=JSON
-      val stubResult = "{\"as\":\"AS24940 Hetzner Online GmbH\",\"city\":\"Nuremberg\",\"country\":\"Germany\",\"countryCode\":\"DE\"}"
+      val stubResult =
+        "{\"as\":\"AS24940 Hetzner Online GmbH\",\"city\":\"Nuremberg\",\"country\":\"Germany\",\"countryCode\":\"DE\"}"
 
       override def httpWeb = Reader[Env, HttpWeb] { _ =>
         val mockHttpWeb = mock[HttpWeb]
 
-        (mockHttpWeb.getUriAsString(_: String))
-          .expects(*).returning(Future.successful(stubResult)).anyNumberOfTimes()
+        (mockHttpWeb
+          .getUriAsString(_: String))
+          .expects(*)
+          .returning(Future.successful(stubResult))
+          .anyNumberOfTimes()
 
         mockHttpWeb
       }
     }
 
     val testIpAddress = "78.47.232.67"
-    
+
     val service = new InfoByIpIp2IpProvider(testEnv)
 
     val futCountryCode = service.countryCode(testIpAddress)
@@ -59,13 +66,17 @@ class GetInfoByIpTest extends FunSpec with Matchers with MockFactory {
 
     object testEnv extends ProductionEnv {
       // language=JSON
-      val stubResult = "{\"ip\":\"78.47.232.67\",\"country_code\":\"DE\",\"country_name\":\"Germany\"}"
+      val stubResult =
+        "{\"ip\":\"78.47.232.67\",\"country_code\":\"DE\",\"country_name\":\"Germany\"}"
 
       override def httpWeb = Reader[Env, HttpWeb] { _ =>
         val mockHttpWeb = mock[HttpWeb]
 
-        (mockHttpWeb.getUriAsString(_: String))
-          .expects(*).returning(Future.successful(stubResult)).anyNumberOfTimes()
+        (mockHttpWeb
+          .getUriAsString(_: String))
+          .expects(*)
+          .returning(Future.successful(stubResult))
+          .anyNumberOfTimes()
 
         mockHttpWeb
       }
@@ -80,12 +91,17 @@ class GetInfoByIpTest extends FunSpec with Matchers with MockFactory {
     Await.result(futCountryCode, 20.seconds) shouldBe "de"
   }
 
-  def futureFailedAfter(duration: FiniteDuration)(implicit actorSystem: ActorSystem) =
-    akka.pattern.after(duration, actorSystem.scheduler)(Future.failed(new Exception))
+  def futureFailedAfter(
+    duration: FiniteDuration
+  )(implicit actorSystem: ActorSystem) =
+    akka.pattern.after(duration, actorSystem.scheduler)(
+      Future.failed(new Exception)
+    )
 
-
-  it("should return fall-back country code if all of the services are not accessible" +
-    "within 3 seconds") {
+  it(
+    "should return fall-back country code if all of the services are not accessible" +
+      "within 3 seconds"
+  ) {
 
     object testEnv extends ProductionEnv {
       override def freeGeoIpProvider: Reader[Env, InfoByIpProvider] =
@@ -111,9 +127,10 @@ class GetInfoByIpTest extends FunSpec with Matchers with MockFactory {
 
     val testIpAddress = "78.47.232.67"
 
-    val service = new GetInfoByIpServiceImpl(testEnv)
+    val service = GetInfoByIpServiceImpl(testEnv)
 
-    val futCountryCode = service.countryCode(testIpAddress, fallbackTimeout = 3.seconds)
+    val futCountryCode =
+      service.countryCode(testIpAddress, fallbackTimeout = 3.seconds)
 
     val FALLBACK_COUNTRY_CODE = "lv"
     Await.result(futCountryCode, 4.seconds) shouldBe FALLBACK_COUNTRY_CODE
@@ -121,7 +138,7 @@ class GetInfoByIpTest extends FunSpec with Matchers with MockFactory {
 
 //    it("integration test") {
 //      val testIpAddress = "78.47.232.67"
-//      val service = new GetInfoByIpServiceImpl(productionEnv)
+//      val service = GetInfoByIpServiceImpl(productionEnv)
 //
 //      val futCountryCode = service.countryCode(testIpAddress, fallbackTimeout = 3.seconds)
 //
